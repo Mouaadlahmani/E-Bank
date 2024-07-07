279ab2723c746ac1329c12e22f0575f1e06c2875
Mouaadlahmani <157011355+Mouaadlahmani@users.noreply.github.com>
2024-07-07 12:36:11 +0100

    Création de compte controller

---
+++ E-Bank/src/main/java/com/mouad/E_Bank/controller/CompteController.java
@@ -0,0 +1,40 @@
+package com.mouad.E_Bank.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mouad.E_Bank.model.Compte;
+import com.mouad.E_Bank.service.CompteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+@RequestMapping("/api/compte")
+public class CompteController {
+
+	@Autowired
+	CompteService compteService;
+	
+	@PostMapping
+	public Compte addAccount(@RequestBody Compte compte) {
+		return compteService.addAccount(compte);
+	}
+	
+	@GetMapping
+	public List<Compte> getAllComptes() {
+		return compteService.getAllAccounts();
+	}
+	
+	@GetMapping("/account/{id}")
+	public Optional<Compte> getCompteById(@PathVariable Long id){
+		return compteService.getCompteById(id);
+	}
+
+}
